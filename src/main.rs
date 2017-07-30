4efe565832df8e604500cf3ae29e906900f96022
modderme123 <modderme123@gmail.com>
2017-07-30 09:55:38 -0700

    Simplify

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+#[derive(Debug,Clone)] 
 struct Synapse {
     weight: f64,
     value: f64,
 }
+#[derive(Debug,Clone)] 
 struct Neuron {
     synapses: Vec<Synapse>,
     derivatives: Vec<f64>,
@@ -33,15 +35,9 @@ impl Neuron {
     }
     fn backProp(&mut self, rate: f64, delta: f64) {}
     fn create(inputs: i32) -> Neuron {
-        let mut synapses: Vec<Synapse> = Vec::with_capacity(inputs as usize);
-        let mut derivatives: Vec<f64> = Vec::with_capacity(inputs as usize);
-        for i in 0..inputs {
-            synapses.push(Synapse {
-                              weight: 0.5,
-                              value: 0.0,
-                          });
-            derivatives.push(1.0);
-        }
+        let synapse = Synapse { weight: 0.5, value: 0.0, };
+        let synapses: Vec<Synapse> = vec![synapse; inputs as usize];
+        let derivatives: Vec<f64> = vec![1.0; inputs as usize];
         Neuron {
             synapses: synapses,
             derivatives: derivatives,
@@ -78,21 +74,15 @@ impl Layer {
         self.derivatives = derivatives;
     }
     fn create(inputs: i32, outputs: i32) -> Layer {
-        let mut neurons: Vec<Neuron> = Vec::with_capacity(outputs as usize);
-        for i in 0..outputs {
-            neurons.push(Neuron::create(inputs));
-        }
-        let mut derivatives: Vec<f64> = Vec::with_capacity(inputs as usize);
-        for i in 0..inputs {
-            derivatives.push(1.0);
-        }
+        let neurons: Vec<Neuron> = vec![Neuron::create(inputs); outputs as usize];
+        let derivatives: Vec<f64> = vec![1.0; inputs as usize];
         Layer {
             neurons: neurons,
             derivatives: derivatives,
         }
     }
 }
-#[derive(Debug)]
+#[derive(Debug)] 
 struct Network {
     layers: Vec<Layer>,
 }
@@ -123,6 +113,6 @@ impl Network {
 struct TrainingPair {}
 fn main() {
     let mut n = Network::create(2, vec![2], 1);
-    //println!("network: {:?}", n);
+    println!("network: {:?}", n);
     println!("eval: {:?}", n.ev(&vec![1.0, 0.0]));
 }
