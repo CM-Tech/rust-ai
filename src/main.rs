e935f39cf1217bdefe7f4025b48b54a7e303ef90
Cole Kissane <coler706@gmail.com>
2017-07-30 10:05:23 -0700

    Just Need To Add Back Prop Func in Network

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ struct Synapse {
     weight: f64,
     value: f64,
 }
-#[derive(Debug,Clone)] 
+//Neuron has input derivatives to effect prev layer and weight derivatives to change weights
 struct Neuron {
     synapses: Vec<Synapse>,
-    derivatives: Vec<f64>,
+    weightDerivatives: Vec<f64>,
+    valueDerivatives: Vec<f64>,
 }
 impl Neuron {
     fn load_inputs(&mut self, inputs: &Vec<f64>) {
@@ -22,29 +23,45 @@ impl Neuron {
         1.0 / (1.0 + sum.exp())
     }
     fn calcDerivatives(&mut self) {
-        let mut derivatives: Vec<f64> = Vec::with_capacity(self.synapses.len());
+        let mut wderivatives: Vec<f64> = Vec::with_capacity(self.synapses.len());
+        let mut vderivatives: Vec<f64> = Vec::with_capacity(self.synapses.len());
         let mut sum: f64 = 0.0;
         for i in 0..self.synapses.len() {
             sum += self.synapses[i].value * self.synapses[i].weight;
         }
         let mut baseGrad: f64 = sum.exp() / (sum.exp() + 1.0) / (sum.exp() + 1.0);
         for i in 0..self.synapses.len() {
-            derivatives.push(self.synapses[i].value * baseGrad);
+            wderivatives.push(self.synapses[i].value * baseGrad);
+            vderivatives.push(self.synapses[i].weight * baseGrad);
         }
-        self.derivatives = derivatives;
+        self.weightDerivatives = wderivatives;
+        self.valueDerivatives =  vderivatives;
     }
-    fn backProp(&mut self, rate: f64, delta: f64) {}
+    fn backProp(&mut self, rate: f64, delta: f64) {
+        for i in 0..self.synapses.len() {
+            self.synapses[i].weight+=delta/self.weightDerivatives[i]*rate;
+        }
+    }
     fn create(inputs: i32) -> Neuron {
-        let synapse = Synapse { weight: 0.5, value: 0.0, };
-        let synapses: Vec<Synapse> = vec![synapse; inputs as usize];
-        let derivatives: Vec<f64> = vec![1.0; inputs as usize];
+        let mut synapses: Vec<Synapse> = Vec::with_capacity(inputs as usize);
+        let mut wderivatives: Vec<f64> = Vec::with_capacity(inputs as usize);
+        let mut vderivatives: Vec<f64> = Vec::with_capacity(inputs as usize);
+        for i in 0..inputs {
+            synapses.push(Synapse {
+                weight: 0.5,
+                value: 0.0,
+            });
+            wderivatives.push(1.0);
+            vderivatives.push(1.0);
+        }
         Neuron {
             synapses: synapses,
-            derivatives: derivatives,
+            weightDerivatives: wderivatives,
+            valueDerivatives:vderivatives
         }
     }
 }
-#[derive(Debug)]
+//Layer Only has one type of derivative to store (to back prop to prev layer) its of the inputs type
 struct Layer {
     neurons: Vec<Neuron>,
     derivatives: Vec<f64>,
@@ -63,12 +80,16 @@ impl Layer {
         }
         out
     }
-    fn calcDerivatives(&mut self) {
-        let in_len: usize = self.neurons[0].synapses.len();
-        let mut derivatives: Vec<f64> = Vec::with_capacity(in_len);
-        for i in 0..in_len {
+    fn backProp(&mut self,rate: f64, deltas: Vec<f64>){
+        for j in 0..self.neurons.len() {
+            self.neurons[j].calcDerivatives();
+            self.neurons[j].backProp(rate,deltas[j]);
+        }
+        let inLen: usize = self.neurons[0].synapses.len();
+        let mut derivatives: Vec<f64> = Vec::with_capacity(inLen);
+        for i in 0..inLen {
             for j in 0..self.neurons.len() {
-                derivatives.push(self.neurons[j].derivatives[i] / (self.neurons.len() as f64));
+                derivatives.push(self.neurons[j].valueDerivatives[i] / (self.neurons.len() as f64)*deltas[j]);
             }
         }
         self.derivatives = derivatives;
@@ -95,8 +116,11 @@ impl Network {
         }
         input
     }
-    fn create(inputs: i32, layer_sizes: Vec<i32>, outputs: i32) -> Network {
-        let mut layers: Vec<Layer> = Vec::with_capacity(2 + layer_sizes.len());
+    fn backProp(&mut self, rate: f64, delta: f64) {
+        
+    }
+    fn create(inputs: i32, layerSizes: &Vec<i32>, outputs: i32) -> Network {
+        let mut layers: Vec<Layer> = Vec::with_capacity(2 + layerSizes.len());
         layers.push(Layer::create(inputs, inputs));
         if layer_sizes.len() > 0 {
             layers.push(Layer::create(inputs, layer_sizes[0]));
